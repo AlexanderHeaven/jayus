63b105c4da0d2ce1c19b342f7ef417c917de73a7
Alexander Heaven <alexanderovheaven@gmail.com>
2016-02-09 14:01:17 +0000

    Store and queue added. Tests for them soon?

--- jayus/protocol_factory.py
+++ jayus/protocol_factory.py
@@ -1,7 +1,15 @@
-class ChatProtoFactory:
+from twisted.internet.protocol import Factory
+
+from collections import deque
+from Queue import Queue
+
+
+class ChatProtoFactory(Factory):
 
     def __init__(self):
-        pass
+        self.users = {}     # Map of users to chat instances
+        self.store = deque({}, 100)    # 100 entry map user to message
+        self.queue = Queue(0)       # Infinitely long queue
 
     def build_chat_protocol(self):
         pass
