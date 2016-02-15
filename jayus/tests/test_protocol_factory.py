a35fad326a9da07edeb7a4948fb4ed477ef4703d
Alexander Heaven <alexanderovheaven@gmail.com>
2016-02-15 16:20:23 +0000

    More test structure. All easy stuff done.

---
+++ jayus/tests/test_protocol_factory.py
@@ -0,0 +1,19 @@
+import unittest
+from jayus.protocol_factory import ChatProtoFactory
+
+
+class TestChatFactory(unittest.TestCase):
+    def setUp(self):
+        self.chat_factory = ChatProtoFactory()
+
+    def test_store_lower(self):
+        pass
+
+    def test_store_upper(self):
+        pass
+
+    def test_queue_storage(self):
+        pass
+
+    def test_queue_behaviour(self):
+        pass
